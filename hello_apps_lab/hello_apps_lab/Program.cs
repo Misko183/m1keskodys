2733ab53976495ec5ba4d05be02fe8ae3554e33b
Misko183 <rtmike183@gmail.com>
2020-09-28 09:44:10 +0200

    Changed welcome message.

---
+++ hello_apps_lab/hello_apps_lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_apps_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Apps Lab!");
+        }
+    }
+}
